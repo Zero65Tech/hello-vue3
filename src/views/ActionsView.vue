d527f3cdabea5dc1050c8ebd81f0ff6a64ee6558
bgargy <138105909+bgargy@users.noreply.github.com>
2023-12-18 22:35:11 +0530

    create, delete ok, update no

--- src/views/ActionsView.vue
+++ src/views/ActionsView.vue
@@ -3,57 +3,110 @@ export default {
   data() {
     return {
       documentList: [],
+      formData: {
+        id: null,
+        first_name: "",
+        last_name: "",
+        collection: null
+      }
     };
   },
   mounted() {
-    // Fetch the document list from the server
-    fetch('http://localhost:3000/users')
-      .then(response => {
-        if (!response.ok) {
-        throw new Error( `HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        this.documentList = data;
-      })
-      .catch(error => {
-        console.error('Error fetching document list:', error);
-        // Handle errors here
-      });
+    
+    this.fetchDocumentList();
   },
   methods: {
+    fetchDocumentList() {
+      fetch('http://localhost:3000/users')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          this.documentList = data;
+        })
+        .catch(error => {
+          console.error('Error fetching document list:', error);
+          // Handle errors here
+        });
+    },
     create() {
-        document.getElementById("add").type="text";
-        document.getElementById("update").type="hidden";
-        
-      },
-      update(){
-        document.getElementById("update").type="text";
-        document.getElementById("add").type="hidden";
-      },
-      delete(){
-        document.getElementById("delete").type="text";
-      }
+      
+      fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(this.formData),
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log('Document added successfully:', data);
+          this.fetchDocumentList(); // Refresh the document list after adding
+        })
+        .catch(error => {
+          console.error('Error adding document:', error);
+          // Handle errors here
+        });
+    },
+    update() {
+      
+      fetch(`http://localhost:3000/users/${this.formData.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(this.formData),
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log('Document updated successfully:', data);
+          this.fetchDocumentList(); 
+        })
+        .catch(error => {
+          console.error('Error updating document:', error);
+          // Handle errors here
+        });
+    },
+    deleteDocument() {
+      
+      fetch(`http://localhost:3000/users/${this.formData.id}`, {
+        method: 'DELETE',
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          console.log('Document deleted successfully');
+          this.fetchDocumentList(); 
+        })
+        .catch(error => {
+          console.error('Error deleting document:', error);
+        });
+    }
   }
 };
 </script>
 
 <template>
-    <div>
-        <nav id="aviewnav">
-            <v-btn :exact="true" variant="flat" :ripple="false" class="ml-4" @click="create">Add</v-btn>
-            <v-btn :exact="true" variant="flat" :ripple="false" class="ml-4" @click="update">Update </v-btn>
-            <v-btn :exact="true" variant="flat" :ripple="false" class="ml-4" @click="delete">Delete</v-btn>
-        </nav>
-        
-    </div>
-    <div><br>
-        <input type="hidden" id="add" value="Input Here"> 
-        <input type="hidden" id="update" value="Update Here">
-        <input type="hidden" id="delete" value="Enter ID to delete"></div>
-    <div>
+  <div>
+    <div style=" text-align: center;">
+      <input style="background-color: lightblue;" v-model="formData.id" type="text" placeholder="ID">
+      <input style="background-color: lightblue;" v-model="formData.first_name" type="text" placeholder="First Name">
+      <input style="background-color: lightblue;" v-model="formData.last_name" type="text" placeholder="Last Name">
+      <input style="background-color: lightblue;" v-model="formData.collection" type="text" placeholder="Collection">
+    </div><br>
+    <nav id="aviewnav" style=" text-align: center;">
+      <v-btn :exact="true" variant="flat" :ripple="false" class="ml-4" @click="create">Add</v-btn>
+      <v-btn :exact="true" variant="flat" :ripple="false" class="ml-4" @click="update">Update </v-btn>
+      <v-btn :exact="true" variant="flat" :ripple="false" class="ml-4" @click="deleteDocument">Delete</v-btn>
+    </nav>
+    <br>
     
+
+    <div>
       <table>
         <thead>
           <tr>
@@ -66,36 +119,36 @@ export default {
           <tr v-for="(document, index) in documentList" :key="index">
             <td>{{ document.id }}</td>
             <td>{{ document.collection }}</td>
-            <td>{{ document.first_name+ document.last_name }}</td>
+            <td>{{ document.first_name + ' ' + document.last_name }}</td>
           </tr>
         </tbody>
       </table>
     </div>
-  </template>
-  
-  <style>
+  </div>
+</template>
+
+<style>
   table {
     width: 100%;
     border-collapse: collapse;
     margin-top: 20px;
+    background-color: gr;
   }
-  
+
   th, td {
     border: 1px solid #131010;
     padding: 8px;
     text-align: left;
   }
-  
+
   th {
     background-color: #131010;
   }
-  #aviewnav{
-   
+
+  #aviewnav {
   }
-  #add{
-    background-color: aliceblue;
+
+  input {
+    margin: 5px;
   }
-  #update{
-    background-color: rgb(203, 229, 252);
-  }
-  </style>
+</style>
