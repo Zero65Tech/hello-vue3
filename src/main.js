446d0180b7187f4b8e66124dc3d5e91bc99e307f
Anurag Patel <118664591+anurag-2024@users.noreply.github.com>
2025-07-17 22:04:41 +0000

    use tailwind and pinia

--- src/main.js
+++ src/main.js
@@ -1,11 +1,30 @@
 import './assets/main.css'
+import './styles.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//PrimeVue
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import ToastService from 'primevue/toastservice';
+import toast from 'primevue/toast'
+import 'primeicons/primeicons.css';
+
 const app = createApp(App)
 
 app.use(router)
+
+app.use(ToastService);
+app.component('AppToast', toast)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: false
+    }
+  }
+});
 
 app.mount('#app')
